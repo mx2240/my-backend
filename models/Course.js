d5f537fec90eaf9dd55d739c5a1a0468b29f91bb
mx2240 <lovemicky10@gmail.com>
2025-11-25 13:24:41 +0000

    up

--- models/Course.js
+++ models/Course.js
@@ -1,25 +1,14 @@
-
-
-
-// const mongoose = require("mongoose");
-
-// const courseSchema = new mongoose.Schema({
-//     title: { type: String, required: true },
-//     description: String,
-//     duration: String,
-// });
-
-// module.exports = mongoose.model("Course", courseSchema);
-
-
 const mongoose = require("mongoose");
 
-const courseSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    code: { type: String, required: true },
-    description: { type: String, required: true },
-    credits: { type: Number, required: true },
-    instructor: { type: String, required: true },
-}, { timestamps: true });
+const courseSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        code: { type: String, required: true, unique: true },
+        description: { type: String, required: true },
+        credits: { type: Number, required: true },
+        instructor: { type: String, required: true }
+    },
+    { timestamps: true }
+);
 
-module.exports = mongoose.model("Course", courseSchema);
+module.exports = mongoose
