1972b362a0148d11da8e58e6259e47218240a572
mx2240 <lovemicky10@gmail.com>
2025-11-25 22:59:36 +0000

    up

--- routes/feesRoutes.js
+++ routes/feesRoutes.js
@@ -5,7 +5,9 @@ const {
     createFee,
     assignFee,
     getStudentsWithFees,
-    updateFeeStatus
+    updateFeeStatus,
+    getAllFees
+
 } = require("../controllers/feesController");
 
 // --- Admin only ---
@@ -13,5 +15,8 @@ router.post("/create", verifyToken, verifyAdmin, createFee);
 router.post("/assign", verifyToken, verifyAdmin, assignFee);
 router.get("/students", verifyToken, verifyAdmin, getStudentsWithFees);
 router.put("/status", verifyToken, verifyAdmin, updateFeeStatus);
+router.get("/all", verifyToken, verifyAdmin, getAllFees);
+
+
 
 module.exports = router;
