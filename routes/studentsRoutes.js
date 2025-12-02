0493fe3b5d444e7e2addf8545b1bce5ae52e9e26
mx2240 <lovemicky10@gmail.com>
2025-12-02 00:17:17 +0000

    up

--- routes/studentsRoutes.js
+++ routes/studentsRoutes.js
@@ -3,6 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const { createStudent, getStudents } = require("../controllers/studentController");
+const { studentLogin } = require("../controllers/studentAuthController");
+
+router.post("/login", studentLogin);
 
 router.post("/", createStudent);
 router.get("/", getStudents);
