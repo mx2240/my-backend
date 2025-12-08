d3d2460ea8699578fc247863633a5a07cb924285
mx2240 <lovemicky10@gmail.com>
2025-12-08 14:18:51 +0000

    up

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,28 +1,41 @@
+// const express = require("express");
+// const router = express.Router();
+// const { verifyToken, requireAdmin } = require("../middleware/verifyToken");
+
+// const {
+//     createEvent,
+//     getEvents,
+//     getEventById,
+//     updateEvent,
+//     deleteEvent
+// } = require("../controllers/eventController");
+
+// // Admin create event
+// router.post("/", verifyToken, requireAdmin, createEvent);
+
+// // Get all events (public or admin)
+// router.get("/", getEvents);
+
+// // Get single event
+// router.get("/:id", getEventById);
+
+// // Update event
+// router.put("/:id", verifyToken, requireAdmin, updateEvent);
+
+// // Delete event
+// router.delete("/:id", verifyToken, requireAdmin, deleteEvent);
+
+// module.exports = router;
+
+
 const express = require("express");
 const router = express.Router();
-const { verifyToken, requireAdmin } = require("../middleware/verifyToken");
+const { createEvent, getEvents } = require("../controllers/eventController");
 
-const {
-    createEvent,
-    getEvents,
-    getEventById,
-    updateEvent,
-    deleteEvent
-} = require("../controllers/eventController");
+// ADMIN: ADD EVENT
+router.post("/create", createEvent);
 
-// Admin create event
-router.post("/", verifyToken, requireAdmin, createEvent);
-
-// Get all events (public or admin)
+// GET ALL EVENTS
 router.get("/", getEvents);
-
-// Get single event
-router.get("/:id", getEventById);
-
-// Update event
-router.put("/:id", verifyToken, requireAdmin, updateEvent);
-
-// Delete event
-router.delete("/:id", verifyToken, requireAdmin, deleteEvent);
 
 module.exports = router;
