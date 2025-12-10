c8dae1c34e2ccf1f6f59abec1cdfc8793331eb82
mx2240 <lovemicky10@gmail.com>
2025-12-10 13:02:36 +0000

    up

--- controllers/StudentPasswordController.js
+++ controllers/StudentPasswordController.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
-const User = require("../models/Student");
+const User = require("../models/user");
 const nodemailer = require("nodemailer");
 
 // SEND EMAIL FUNCTION
